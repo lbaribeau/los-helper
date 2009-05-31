9280d6e6024aef3b016022de4cfe861ab56f71a1
Kevin Baribeau <kevin.baribeau@gmail.com>
2009-05-31 03:25:49 +0000

    * remove (unnecessary) iterator variable from for loop * condense nested loop into list comprehension

--- main/MudReaderThread.py
+++ main/MudReaderThread.py
@@ -748,83 +748,74 @@ class MudReaderThread ( threading.Thread ):
         inv_string = self.replace_newlines_with_spaces(inv_string)
         # Use split to break into a list of strings
         inv_list = inv_string.split(',')
-        for i in range(0, len(inv_list)):
-            inv_list[i] = inv_list[i].lstrip()
-            inv_list[i] = inv_list[i].rstrip()
+        for item in inv_list:
+            item = item.lstrip()
+            item = item.rstrip()
             # How should I do quantities?  It probably makes sense
             # just to have duplicate items in the actual list
-            if(inv_list[i][0:2] == "a "):
-                inv_list[i] = inv_list[i][2:]
-                return_list.append(inv_list[i])
-            elif(inv_list[i][0:3] == "an "):
-                inv_list[i] = inv_list[i][3:]
-                return_list.append(inv_list[i])
-            elif(inv_list[i][0:5] == "some "):
-                inv_list[i] = inv_list[i][5:]
-                return_list.append(inv_list[i])
+            if(item[0:2] == "a "):
+                item = item[2:]
+                return_list.append(item)
+            elif(item[0:3] == "an "):
+                item = item[3:]
+                return_list.append(item)
+            elif(item[0:5] == "some "):
+                item = item[5:]
+                return_list.append(item)
             # In doing quantities, may as well use insert and keep
             # the list ordered.
-            elif(inv_list[i][0:4] == "two "):
-                if(inv_list[i][0:12] == "two sets of "):
-                    inv_list[i] = inv_list[i][12:]
+            elif(item[0:4] == "two "):
+                if(item[0:12] == "two sets of "):
+                    item = item[12:]
                 else:
-                    inv_list[i] = inv_list[i][4:]
+                    item = item[4:]
                     # Remove the 's' if its there
-                    if(inv_list[i][len(inv_list[i])-1] == 's'):
-                        if(inv_list[i][len(inv_list[i])-3:] == "ses" or
-                           inv_list[i][len(inv_list[i])-3:] == "xes"):
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-2]
+                    if(item[len(item)-1] == 's'):
+                        if(item[len(item)-3:] == "ses" or
+                           item[len(item)-3:] == "xes"):
+                            item = item[:len(item)-2]
                         else:
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-1]
-                for j in range(0,2):
-                    return_list.append(inv_list[i])
-                #inv_list.insert(i, inv_list[i]) # check this
-            elif(inv_list[i][0:6] == "three "):
-                if(inv_list[i][0:14] == "three sets of "):
-                    inv_list[i] = inv_list[i][14:]
+                            item = item[:len(item)-1]
+                return_list = return_list + [item for x in range(0,2)]
+            elif(item[0:6] == "three "):
+                if(item[0:14] == "three sets of "):
+                    item = item[14:]
                 else:
-                    inv_list[i] = inv_list[i][6:]
+                    item = item[6:]
                     # Remove the 's' if its there
-                    if(inv_list[i][len(inv_list[i])-1] == 's'):
-                        if(inv_list[i][len(inv_list[i])-3:] == "ses" or
-                           inv_list[i][len(inv_list[i])-3:] == "xes"):
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-2]
+                    if(item[len(item)-1] == 's'):
+                        if(item[len(item)-3:] == "ses" or
+                           item[len(item)-3:] == "xes"):
+                            item = item[:len(item)-2]
                         else:
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-1]
-                for j in range(0,3):
-                    #inv_list.insert(inv_list[i], i) # check this
-                    return_list.append(inv_list[i])
-            elif(inv_list[i][0:5] == "four "):
-                if(inv_list[i][0:13] == "four sets of "):
-                    inv_list[i] = inv_list[i][13:]
+                            item = item[:len(item)-1]
+                return_list = return_list + [item for x in range(0,3)]
+            elif(item[0:5] == "four "):
+                if(item[0:13] == "four sets of "):
+                    item = item[13:]
                 else:
-                    inv_list[i] = inv_list[i][5:]
+                    item = item[5:]
                     # Remove the 's' if its there
-                    if(inv_list[i][len(inv_list[i])-1] == 's'):
-                        if(inv_list[i][len(inv_list[i])-3:] == "ses" or
-                           inv_list[i][len(inv_list[i])-3:] == "xes"):
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-2]
+                    if(item[len(item)-1] == 's'):
+                        if(item[len(item)-3:] == "ses" or
+                           item[len(item)-3:] == "xes"):
+                            item = item[:len(item)-2]
                         else:
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-1]
-                for j in range(0,4):
-                    #inv_list.insert(inv_list[i], i) # check this
-                    return_list.append(inv_list[i])
-                #sys.stdout.write(str(inv_list[i]) + "\n")
-            elif(inv_list[i][0:5] == "five "):
-                if(inv_list[i][0:13] == "five sets of "):
-                    inv_list[i] = inv_list[i][13:]
+                            item = item[:len(item)-1]
+                return_list = return_list + [item for x in range(0,4)]
+            elif(item[0:5] == "five "):
+                if(item[0:13] == "five sets of "):
+                    item = item[13:]
                 else:
-                    inv_list[i] = inv_list[i][5:]
+                    item = item[5:]
                     # Remove the 's' if its there
-                    if(inv_list[i][len(inv_list[i])-1] == 's'):
-                        if(inv_list[i][len(inv_list[i])-3:] == "ses" or
-                           inv_list[i][len(inv_list[i])-3:] == "xes"):
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-2]
+                    if(item[len(item)-1] == 's'):
+                        if(item[len(item)-3:] == "ses" or
+                           item[len(item)-3:] == "xes"):
+                            item = item[:len(item)-2]
                         else:
-                            inv_list[i] = inv_list[i][:len(inv_list[i])-1]
-                for j in range(0,5):
-                    #inv_list.insert(inv_list[i], i) # check this
-                    return_list.append(inv_list[i])
+                            item = item[:len(item)-1]
+                return_list = return_list + [item for x in range(0,5)]
             # TBD go up to fifteen.  Maybe find a more scalable way to
             # do it.
             else:
