7dec0f5f76810429ec03b3dc5e668ce06de5cc89
Laurier Baribeau <laurier.baribeau@gmail.com>
2014-04-06 10:49:58 -0400

    Fix: WinConsoleHandler was throwing an error due to being imported when not on win32 platform.
    Also made some misc improvements - inheriting object and removing the albeit cool import from __future__, and whitespace edits.

--- main/WinConsoleHandler.py
+++ main/WinConsoleHandler.py
@@ -1,10 +1,12 @@
 import ctypes
+
 # I GOT THIS FROM   http://code.activestate.com/recipes/496901/
 # Was hard to find.
 
 # See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winprog/winprog/windows_api_reference.asp
 # for information on Windows APIs.
-class WinConsoleHandler:
+
+class WinConsoleHandler(object):
 	
 	STD_INPUT_HANDLE = -10
 	STD_OUTPUT_HANDLE= -11
@@ -30,38 +32,45 @@ class WinConsoleHandler:
 		"""
 		bool = ctypes.windll.kernel32.SetConsoleTextAttribute(self.__std_out_handle, color)
 		return bool
-# set_color
 
 	def black(self):
 		self.set_color(0x00 | self.__brightness)
 		return
+
 	def blue(self):
 		self.set_color(self.FOREGROUND_BLUE | self.__brightness)
 		return
+
 	def green(self):
 		self.set_color(self.FOREGROUND_GREEN | self.__brightness)
 		return
+
 	def cyan(self):
 		self.set_color(0x03 | self.__brightness)
 		return
+
 	def red(self):
 		self.set_color(self.FOREGROUND_RED | self.__brightness)
 		return
+
 	def magenta(self):
 		self.set_color(0x05 | self.__brightness)
 		return
+
 	def yellow(self):
 		self.set_color(0x06 | self.__brightness)
 		return
+
 	def white(self): 
 		self.set_color(0x07 | self.__brightness)
 		return    
 
-# Hopefully not misleading - you have to call these BEFORE calling colors to
-# have an effect.. default is dark.
+	# Hopefully not misleading - you have to call these BEFORE calling colors to
+	# have an effect.. default is dark.
 	def set_bright(self):                      
 		self.__brightness = 0x08
 		return
+
 	def set_dark(self):
 		self.__brightness = 0x00
 		return
