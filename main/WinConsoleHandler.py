694707fbb6b58c1f3050199f264732abede2e240
Adam B <gadimus@gmail.com>
2014-04-08 10:37:03 -0600

    Overwrote file from Dev34
    Weird indentation error.  I'm not sure why this keeps happening...

--- main/WinConsoleHandler.py
+++ main/WinConsoleHandler.py
@@ -12,18 +12,18 @@ class WinConsoleHandler(object):
 	STD_OUTPUT_HANDLE= -11
 	STD_ERROR_HANDLE = -12
 
-    FOREGROUND_BLUE = 0x01 # text color contains blue.
-    FOREGROUND_GREEN= 0x02 # text color contains green.
-    FOREGROUND_RED  = 0x04 # text color contains red.
-    FOREGROUND_INTENSITY = 0x08 # text color is intensified.
-    BACKGROUND_BLUE = 0x10 # background color contains blue.
-    BACKGROUND_GREEN= 0x20 # background color contains green.
-    BACKGROUND_RED  = 0x40 # background color contains red.
-    BACKGROUND_INTENSITY = 0x80 # background color is intensified.
+	FOREGROUND_BLUE = 0x01 # text color contains blue.
+	FOREGROUND_GREEN= 0x02 # text color contains green.
+	FOREGROUND_RED  = 0x04 # text color contains red.
+	FOREGROUND_INTENSITY = 0x08 # text color is intensified.
+	BACKGROUND_BLUE = 0x10 # background color contains blue.
+	BACKGROUND_GREEN= 0x20 # background color contains green.
+	BACKGROUND_RED  = 0x40 # background color contains red.
+	BACKGROUND_INTENSITY = 0x80 # background color is intensified.
 
-    __brightness = 0x00;
+	__brightness = 0x00;
 
-    __std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+	__std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
 
 	def set_color(self, color):
 		"""(color) -> BOOL
