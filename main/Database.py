c88fa64b1e460913a9596e83c679c2bbf5585976
Adam B <gadimus@gmail.com>
2014-04-09 15:11:57 -0600

    Database Overhaul
    The table objects now include map() functions inside of them to handle
    the business logic needed to properly match or add content to the
    database.

--- main/Database.py
+++ main/Database.py
@@ -8,15 +8,85 @@ db = SqliteDatabase("los" + str(misc_functions.VERSION) + ".db")
 db.connect()
 
 class BaseModel(Model):
+    def __eq__(self, other): #simple type check
+        if type(other) is type(self):
+            return True
+        else:
+            return False
+
     class Meta:
         database = db
 
 class Area(BaseModel):
     name = CharField() #Chapel
     description = CharField(null=True) #This will only be used for crawler comparisons
+    is_always_dark = BooleanField(default=False)
+    is_dark_at_night = BooleanField(default=False)
+    is_restorative = BooleanField(default=False)
 
-    def toString(self):
+    '''Private Area Functions'''
+    def map(self, directions, area_from=None, direction_from=None):
+        is_new_mapping = False
+
+        for direction in directions:
+            direction.map() # this will update our direction objects with their corresponding ids
+
+        matching_areas = Area.get_areas_by_name_and_description(self.name, self.description)
+
+        for area in matching_areas: #we selected based on name / description so we know they match but just in case or if our descriptions are null
+            if (area.has_directions(directions)):
+                self = area
+                return is_new_mapping
+
+        is_new_mapping = True
+        super(Area, self).save()
+
+        #now we map our area links
+        for direction in directions:
+            area_link = AreaLink(area_from=self.id, area_to=None, direction_type=direction)
+
+            '''if (direction_from.opposite is None):
+                if (direction.id == direction_from.opposite.id):
+                    area_link.map(area_from, direction_from)
+                else:'''
+
+            area_link.map()
+
+        return is_new_mapping
+
+    def has_directions(self, directions): #receiving mapped directions with IDs so we just do a compare on the Area Links
+        area_links = AreaLink.get_area_links_from_area(self)
+        has_directions = (area_links.count() == len(directions))
+
+        #print("has_directions: " + str(has_directions) + " area: " + str(area.to_string()))
+
+        if (has_directions):
+            direction_found = False
+            for direction_type in directions:
+                direction_found = False
+                for area_link in area_links:
+                    if (direction_type == area_link.direction_type):
+                        direction_found = True
+                if not direction_found:
+                    return False
+            has_directions = direction_found
+
+        return has_directions
+
+    def to_string(self):
         return str(self.id) + ", " + self.name + ", " + str(self.description)
+
+    '''Static Area Functions'''
+    def get_areas_by_name_and_description(area_name, area_description):
+        areas = []
+
+        try:
+            areas = Area.select().where((Area.name == area_name) & (Area.description == area_description))
+
+        except Area.DoesNotExist:
+            areas = []
+
+        return areas
 
 class DirectionType(BaseModel):
     name = CharField() #ex. north, out, door
@@ -24,207 +94,117 @@ class DirectionType(BaseModel):
     primer = CharField(null=True) #ex. open %s, unlock %s key
     opposite = ForeignKeyField('self', null=True)
 
-    def toString(self):
+    '''Private Area Functions'''
+    def map(self):
+        is_new_mapping = False
+
+        direction_type = DirectionType.get_direction_type_by_name_and_opposite(self)
+
+        if (direction_type is not None): #in this case we've discovered a new direction
+            self = direction_type
+            return is_new_mapping
+
+        super(DirectionType, self).save()
+        is_new_mapping = True
+
+        return is_new_mapping
+
+    def to_string(self):
         return str(self.id) + ", " + self.name + ", " + str(self.primer) + ", " + str(self.opposite)
 
-class AreaLink(BaseModel):
-    areaFrom = ForeignKeyField(Area, related_name='areaFrom') #id is a default attribute
-    areaTo = ForeignKeyField(Area, related_name='areaTo', null=True) #id is a default attribute
-    directionType = ForeignKeyField(DirectionType)
+    '''Static DirectionType Functions'''
+    def get_direction_type_by_name_and_opposite(direction_type): #this should always be unique
+        direction_types = None
 
-    def getAreaToName(self):
-        if (self.areaTo is not None):
-            return self.areaTo.name
+        try:
+            direction_types = DirectionType.select().where((DirectionType.name == direction_type.name) &
+             (DirectionType.opposite == direction_type.opposite)).get()
+
+        except DirectionType.DoesNotExist:
+            direction_types = None
+
+        return direction_types
+
+class AreaLink(BaseModel):
+    area_from = ForeignKeyField(Area, related_name='area_from') #id is a default attribute
+    area_to = ForeignKeyField(Area, related_name='area_to', null=True) #id is a default attribute
+    direction_type = ForeignKeyField(DirectionType)
+    is_useable = BooleanField(default=True) #if the link is broken or potentially harzardous we don't want to use it
+
+    '''Private Area Functions'''
+    def map(self, area_from=None, direction_from=None):
+        is_new_mapping = False
+
+        if area_from is not None and direction_from is not None:
+            if self.direction_type == direction_from.opposite:
+                area_link.area_to = area_from
+            else:
+                '''code a case to handle this direction issue on opposites (west opposite out)
+                since most of the direction types are likely going to have hardcoded opposite at first
+                and then especially for exceptions like this it might not be worth the effort
+                A github issue will be made'''
+
+        super(AreaLink, self).save()
+
+
+        return is_new_mapping
+
+    def get_area_to_name(self):
+        if (self.area_to is not None):
+            return self.area_to.name
         else:
             return None
 
-    def toString(self):
-        return str(self.id) + ", " + str(self.areaFrom.name) + ", " + str(self.getAreaToName()) + ", " + str(self.directionType.name)
+    def to_string(self):
+        return str(self.id) + ", " + str(self.area_from.name) + ", " + str(self.get_area_to_name()) + ", " + str(self.direction_type.name)
 
-def mapArea(area, directions, areaFrom=None, directionFrom=None):
-    #first things first - we want to get the ids of these directions
-    #this way we can compare the AreaLinks of the given Area
-    #also if we discover a brand new direction we've guaranteed the area is new
-    isNew = False
-    dTypes, includesNewDirection = mapDirections(directions)
+    '''Static AreaLink Functions'''
+    def get_area_links_from_area(area):
+        try:
+            area_links = AreaLink.select().where((AreaLink.area_from == area.id))
+        except AreaLink.DoesNotExist:
+            area_links = []
 
-    output = None
-    obj = getAreaByNameAndDirections(area, dTypes)
+        return area_links
 
-    if (obj is None or includesNewDirection): #we're definitely creating a new area
-        area.save()
-        output = area
-        isNew = True
+class Mob(BaseModel):
+    name = CharField()
+    level = IntegerField(null=True)
+    approximate_level = IntegerField(null=True)
+    difficulty_rating = IntegerField(null=True)
+    aura = IntegerField(null=True)
+    is_hostile = BooleanField(default=False)
+    blocks_exit = BooleanField(default=False)
+    blocks_pickup = BooleanField(default=False)
 
-        #only add new links to new areas
-        aLinks, includesNewLinks = mapLinks(output, dTypes)
-    else: #this area seems to already exist
-        output = obj
+    '''Private Mob Functions'''
+    def map(self):
+        return
 
-    #if (areaFrom is not None and directionFrom is not None):
-    #update the AreaLink with the correct areaTo id
-    #mapAreaLink(areaFrom, directionFrom, output) #outputIsOurArea to :)
+    def to_string(self):
+        return str(self.id) + ", " + str(self.name) + ", " + str(self.level)
 
-    return output
+    '''Static Mob Functions'''
 
-def mapDirections(directions):
-    includesNewDirection = False
-    dTypes = []
+class AreaMobs(BaseModel):
+    area = ForeignKeyField(Area)
+    mob = ForeignKeyField(Mob)
 
-    for directionType in directions:
-        dType, isNew = mapDirectionType(directionType)
-        dTypes.append(dType)
+    '''Private Mob Functions'''
+    def map(self):
+        return
 
-        if (isNew):
-            includesNewDirection = True
+    def to_string(self):
+        return str(self.id) + ", " + str(self.area.name) + ", " + str(self.mob.name)
 
-    return (dTypes, includesNewDirection)
-
-def mapDirectionType(directionType):
-    isNew = False
-    obj = getDirectionTypeByName(directionType.name)
-
-    if (obj is None):
-        directionType.save()
-    else:
-        directionType = obj
-
-    return (directionType, isNew)
-
-def mapLinks(areaFrom, directions):
-    includesNewLinks = False
-    aLinks = []
-
-    for directionType in directions:
-        aLink, isNew = mapAreaLink(areaFrom, directionType)
-        aLinks.append(aLink)
-
-        if (isNew):
-            includesNewLinks = True
-
-    return (aLinks, includesNewLinks)
-
-def mapAreaLink(areaFrom, direction, areaTo=None):
-    isNew = False
-    temp = genAreaLink(areaFrom, direction, areaTo)
-
-    obj = getAreaLink(temp, direction)
-
-    if (obj is None):
-        temp.save()
-        isNew = True
-    else:
-        temp = obj
-
-        if (areaTo is not None and temp.areaTo is None):
-            temp.areaTo = areaTo
-            temp.save() #update the area :)
-
-    return (temp, isNew)
-
-def genAreaLink(areaFrom, direction, areaTo=None):
-    return AreaLink(areaFrom=areaFrom, areaTo=areaTo, directionType=direction)
-
-def getAreaByNameAndDirections(area, dTypes):
-    try:
-        temp = Area.select().where(Area.name == area.name)
-        tempDirectionsMatch = False #I'm an optimist
-        output = None
-
-        #if we see more than one area returned from this search we need to loop through
-        for curArea in temp:
-            tempBool = areaHasDirections(curArea, dTypes)
-
-            print(curArea.toString() + " == " + area.toString() + " == " + str(tempBool))
-
-            tempDirectionsMatch = areaHasDirections(curArea, dTypes)
-            if (tempDirectionsMatch):
-                return curArea #this is kind of evil since I'm replacing a big list of Area's with a single area but SHUTUP
-
-    except Area.DoesNotExist:
-        output = None
-
-    return output
-
-def areaHasDirections(area, directions):
-    tempDirections = getAreaDirections(area)
-    #check to see if the number of directions in both lists are the same
-    hasDirections = (len(tempDirections) == len(directions))
-
-    #print("hasDirections: " + str(hasDirections) + " area: " + str(area.toString()))
-
-    if (hasDirections):
-        dNameAInListB = False
-        for dTypeA in directions:
-            dNameAInListB = False
-            for dTypeB in tempDirections:
-                if (dTypeA.name == dTypeB.name):
-                    dNameAInListB = True
-            if not dNameAInListB:
-                return False
-        hasDirections = dNameAInListB
-
-    return hasDirections
-
-def getAreaDirections(area):
-    areaDirections = []
-
-    aLinks = getAreaLinksFromArea(area)
-    areaDirections = getDirectionsFromAreaLinks(aLinks)
-
-    return areaDirections
-
-def getAreaLinksFromArea(areaFrom):
-    try:
-        temp = AreaLink.select().where((AreaLink.areaFrom == areaFrom))
-    except AreaLink.DoesNotExist:
-        temp = None
-
-    return temp
-
-def getDirectionsFromAreaLinks(areaLinks):
-    directions = []
-
-    try:
-        for aLink in areaLinks:
-            directions.append(getDirectionByID(aLink.directionType))
-    except DirectionType.DoesNotExist:
-        #This shouldn't happen but stranger things have...
-        directions = []
-
-    return directions
-
-def getDirectionTypeByName(directionTypeName):
-    try:
-        temp = DirectionType.select().where(DirectionType.name == directionTypeName).get()
-    except DirectionType.DoesNotExist:
-        temp = None
-
-    return temp
-
-def getDirectionByID(directionID):
-    try:
-        temp = DirectionType.select().where(DirectionType.id == directionID).get()
-    except DirectionType.DoesNotExist:
-        temp = None
-        print("Database select error in getDirectionsFromAreaLinks - direction data missing")
-        raise DirectionType.DoesNotExist
-
-    return temp
-
-def getAreaLink(areaFrom, direction):
-    try:
-        temp = AreaLink.select().where((AreaLink.areaFrom == areaFrom) & (AreaLink.directionType == direction)).get()
-    except AreaLink.DoesNotExist:
-        temp = None
-
-    return temp
+    '''Static Mob Functions'''
 
 def create_tables():
     try:
         Area.create_table()
         AreaLink.create_table()
         DirectionType.create_table()
+        #Mob.create_table()
     except:
         pass
 
@@ -233,5 +213,6 @@ def drop_tables():
         Area.drop_table()
         AreaLink.drop_table()
         DirectionType.drop_table()
+        #Mob.drop_table()
     except:
         pass
--- main/DatabaseTest.py
+++ main/DatabaseTest.py
@@ -8,19 +8,19 @@ def main():
     print ("Outputting Direction Types in the Database")
 
     for directionType in DirectionType.select():
-        print ("    " + directionType.toString())
+        print ("    " + directionType.to_string())
 
     print ("\nOutputting Areas in the Database")
 
     for area in Area.select():
-        print ("    " + area.toString())
+        print ("    " + area.to_string())
 
     print ("\nOutputting Area Links in the Database")
 
     for aLink in AreaLink.select():
-        print ("    " + aLink.toString())
+        print ("    " + aLink.to_string())
 
-    #print ("   Area From: " + Area1.toString() + "\n   Area To: " + Area2.toString())
+    #print ("   Area From: " + Area1.to_string() + "\n   Area To: " + Area2.to_string())
 
 def create_database():
     drop_tables()
@@ -30,24 +30,25 @@ def create_database():
 def create_dataset():
     dirOut = DirectionType(name='out')
     dirChapel = DirectionType(name='chapel')
-    dirNorth = DirectionType(name='n')
-    dirSouth = DirectionType(name='s')
-    dirTaco = DirectionType(name='taco', primer='salivate')
+    dirNorth = DirectionType(name='north')
+    dirSouth = DirectionType(name='south')
+    dirTaco = DirectionType(name='door')
     dirSouth2 = DirectionType(name='south')
-    dirNorth2 = DirectionType(name='ne')
+    dirNorth2 = DirectionType(name='northeast')
 
     areaChapel = Area(name='Chapel', description='Pithy Chapel of sorts')
     areaHollyLane = Area(name='Holly Lane', description='Here be Acolytes')
 
-    mapArea(areaChapel, [dirOut])
-    mapArea(areaHollyLane, [dirNorth, dirChapel, dirSouth])
+    areaChapel.map([dirOut])
+    areaHollyLane.map([dirNorth, dirChapel, dirSouth])
 
     areaHollyLane2 = Area(name='Holly Lane', description='Here be MORE Acolytes')
-    mapArea(areaHollyLane2, [dirNorth])
+    areaHollyLane2.map([dirNorth])
 
     areaSpain = Area(name='Spain', description='Rawr sauce')
-
     directions = [dirTaco, dirSouth, dirNorth2]
 
-    savedArea = mapArea(areaSpain, directions)
+    areaSpain.map(directions)
+
+
 main()
