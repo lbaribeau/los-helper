61bb70aef56b8e4d648b215ffbe0606512006ab3
Adam B <gadimus@gmail.com>
2014-04-10 10:48:06 -0600

    Didn't have the fix syntax correct so i removed some comments
    This fixes lbaribea/los-helper#16
    
    Huzzah!

--- main/Cartography.py
+++ main/Cartography.py
@@ -13,7 +13,6 @@ class Cartography(BotReaction):
 
     def __init__(self, mudReaderHandler, commandHandler, character):
         #                      Title        Exit list               Monsters (opt)  Items (opt)
-        #(\n\rObvious exits: .+?[\n\r]?.+?\.)(\n\rYou see .+?[\n\r]?.+?\.)?(\n\rYou see .+?[\n\r]?.+?\.)?
         self.area_with_mobs = "(.*\n\r)(\n\rObvious exits: .+?[\n\r]?.+?\.)\n\r(You see .+?[\n\r]?.+?\.)?[\n\r]?(You see .+?[\n\r]?.+?\.)?"
         self.db = db
 
