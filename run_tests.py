87657e1a05b522dfa8b11ae18bc3f620dc8435a1
Adam Bissonnette <info@mediamanifesto.com>
2015-01-26 13:17:25 -0600

    Runs new tests

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ import_subdir("data")
 from misc_functions import *
 
 #from test.MudReaderThreadTest import MudReaderThreadTest
-#from test.misc_functionsTest import misc_functionsTest #out of date
+from test.misc_functionsTest import misc_functionsTest #out of date
 #from test.BotThreadTest import BotThreadTest
 
 if __name__=='__main__':
@@ -17,8 +17,8 @@ if __name__=='__main__':
     #suite = unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(MudReaderThreadTest))
     #unittest.TextTestRunner().run(suite)
     
-    #suite = unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(misc_functionsTest))
-    #unittest.TextTestRunner().run(suite)
+    suite = unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(misc_functionsTest))
+    unittest.TextTestRunner().run(suite)
         
     #suite = unittest.TestSuite(unittest.TestLoader().loadTestsFromTestCase(BotThreadTest))
     #unittest.TextTestRunner().run(suite)
