478bbfc941773d3d9ec6a31316ee07f263fec1e9
Adam Bissonnette <info@mediamanifesto.com>
2015-01-29 13:22:05 -0600

    Added to string and get by id to base model
    Base model now has a default to_string and other functions to help with
    output.

--- main/data/BaseModel.py
+++ main/data/BaseModel.py
@@ -11,3 +11,21 @@ class BaseModel(Model):
 
     class Meta:
         database = db
+
+    def to_string(self):
+        return str(self.id)
+
+    def __str__(self):
+        return self.to_string()
+
+    def __repr__(self):
+        return self.to_string()
+
+    '''Static BaseModel Functions'''
+    def get_by_id(id):
+        try:
+            obj = BaseModel.select().where((BaseModel.id == id)).get()
+        except BaseModel.DoesNotExist:
+            obj = None
+
+        return obj
