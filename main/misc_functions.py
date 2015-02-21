771a3a121f2e7b6d6f716c8effac08dc052e435b
Adam Bissonnette <info@mediamanifesto.com>
2015-02-21 13:16:40 -0600

    Added show_hidden flag to magentaprint
    show_hidden escapes newline and other characters in the given string

--- main/misc_functions.py
+++ main/misc_functions.py
@@ -72,8 +72,11 @@ def my_list_equal(listA, listB):
 
     return True
     
-def magentaprint(text, is_debug_command=True, log_output=False):
+def magentaprint(text, is_debug_command=True, log_output=False, show_hidden=False):
     global debugMode
+
+    if show_hidden:
+        text = repr(text) #escape all characters in string
 
     if (debugMode or not is_debug_command):
         do_magentaprint (text)
