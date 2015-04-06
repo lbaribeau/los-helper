43df931646700b9a781f234f6ecca4b04aedaa71
lbaribeau <laurier.baribeau@gmail.com>
2015-04-06 19:22:48 -0400

    MudEvent and RegexStore.

---
+++ main/RegexStore.py
@@ -0,0 +1,13 @@
+you_have = ["You have (.+?)\."]
+
+cast_error = [
+    r"That spell does not exist\.",
+    r"You don't know that spell\.",
+    r"Spell name is not unique\.",
+    r"Cast what\?",
+    r"They are not here\.",
+    r"Cast at whom\?" 
+]
+
+hastened = ["You feel yourself moving faster\."]
+feel_slower = ["You feel slower\."]
