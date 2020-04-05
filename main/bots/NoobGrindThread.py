b1bd50f2196ddbd400a08532b999c9eceed671d2
Adam Bissonnette <adam@mediamanifesto.com>
2020-04-05 15:57:21 -0600

    30 second polling

--- main/bots/NoobGrindThread.py
+++ main/bots/NoobGrindThread.py
@@ -7,7 +7,7 @@ class NoobGrindThread(TrackGrindThread):
     def __init__(self, character=None, command_handler=None, mud_reader_handler=None,
                 mud_map=None):
         super().__init__(character, command_handler, mud_reader_handler, mud_map, 0)
-        self.track = ['unlock', 'east', 'west']
+        self.track = ['purchase_key', 'unlock_south', 'south', 'get_book', 'trade_book', 'unlock_east', 'east', 'west']
         self.character.MONSTER_KILL_LIST = ['skeleton']
 
     def stop(self):
@@ -20,7 +20,21 @@ class NoobGrindThread(TrackGrindThread):
       magentaprint("rawr")  
 
     def do_go_hooks(self, exit_str):
-      if exit_str == "unlock":
+      if exit_str == "purchase_key":
+        self.commandHandler.process("purchase key guide")
+        return True
+      elif exit_str == "unlock_south":
+        self.commandHandler.process("unlock south grey")
+        self.commandHandler.process("drop grey yes")
+        return True
+      elif exit_str == "get_book":
+        self.commandHandler.process("get lar")
+        return True
+      elif exit_str == "unlock_south":
+        self.commandHandler.process("unlock south grey")
+        self.commandHandler.process("drop grey yes")
+        return True
+      elif exit_str == "unlock_east":
         self.commandHandler.process("unlock east wood")
         return True
       else:
