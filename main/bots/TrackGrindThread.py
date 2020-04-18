8667ff51db6752638292387544e487edd2ec2889
Adam Bissonnette <adam@mediamanifesto.com>
2020-04-18 13:09:17 -0600

    randomized track again

--- main/bots/TrackGrindThread.py
+++ main/bots/TrackGrindThread.py
@@ -34,7 +34,7 @@ class TrackGrindThread(GrindThread):
         if isinstance(starting_path, int) and starting_path < self.__TOTALPATHS:
             self.__nextpath = starting_path
         else:
-            self.__nextpath = 6#random.randrange(0, self.__TOTALPATHS)
+            self.__nextpath = random.randrange(0, self.__TOTALPATHS)
 
         self.LIMBO_TO_CHAPEL = ["ame", "out", "w", "n", "chapel"]
 
