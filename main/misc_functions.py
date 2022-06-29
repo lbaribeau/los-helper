8f26b91c7248f2b27432f33dd493ce5ff7ddf2d2
Adam Bissonnette <adam@adams-mbp.lan>
2022-06-28 18:41:35 -0600

    updated maplos db and reporting json dump

--- main/misc_functions.py
+++ main/misc_functions.py
@@ -135,7 +135,7 @@ def flush_input():
             msvcrt.getch()
 
 def output_api_feed(filename, data):
-    with open('../reporting_website/api/{}.json'.format(filename), 'w') as outfile:
+    with open('reporting_website/api/{}.json'.format(filename), 'w') as outfile:
         json.dump(data, outfile)
 
 ######
