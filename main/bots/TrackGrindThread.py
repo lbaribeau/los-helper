6076b3532040d12e76120591c83457265ac8c5b6
Adam Bissonnette <adam@adams-mbp.lan>
2022-07-31 20:24:18 -0600

    Removed orcs because it keeps breaking non-darkseers

--- main/bots/TrackGrindThread.py
+++ main/bots/TrackGrindThread.py
@@ -432,7 +432,7 @@ class TrackGrindThread(GrindThread):
             Track("Gnolls", self.smart_gnoll_cave, 11, 20, -1),
             Track("Olmer", self.OLMER, 11, 20, -1),
             Track("Cheryn", self.CHERYN, 11, 15, -1),
-            Track("Orcs", self.ORCS, 11, 20, -1),
+            # Track("Orcs", self.ORCS, 11, 20, -1),
             Track("Artificers", self.ARTIFICERS, 11, 20, -1),
             # Track("Foundry", self.FOUNDRY, 16, 20, 0), #Rimark joins in, not enough mobs actually are there by default
             Track("Rancher Sentries", self.smart_rancher_path, 12, 20, 1),
