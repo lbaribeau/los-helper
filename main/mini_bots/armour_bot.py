f212614c46c24f89b9c174259d977213d1b340bb
Adam Bissonnette <adam@adams-mbp.lan>
2023-07-23 20:49:35 -0600

    Reverted assassin back to chain added new character notes

--- main/mini_bots/armour_bot.py
+++ main/mini_bots/armour_bot.py
@@ -407,10 +407,10 @@ class ArmourBot(MiniBot):
         return self.char.class_string in ['']
 
     def leather(self):
-        return self.char.class_string in ['Thi', 'Ass'] # can wear chain/ringmail but it messes with dodge, hide, backstab
+        return self.char.class_string in ['Thi'] # can wear ringmail but it messes with dodge, hide, backstab
 
     def chain(self):
-        return self.char.class_string in ['Ran', 'Cle', 'Dru']
+        return self.char.class_string in ['Ran', 'Cle', 'Ass', 'Dru']
 
     def steel(self):
         #magentaprint("ArmourBot.steel() class string is: " + str(self.char.class_string))
